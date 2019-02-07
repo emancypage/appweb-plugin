a459273ea895537a2f1ccf422a22784d55dda612
Szymon Szymkowiak <szymkowiak.szymon@gmail.com>
2019-02-07 22:06:16 +0100

    change dir and name of the script and styles

--- Application/Base/Enqueue.php
+++ Application/Base/Enqueue.php
@@ -12,7 +12,7 @@ class Enqueue
 	
 	function enqueue() {
 		// enqueue all our scripts
-		wp_enqueue_style('appweb_pluginstyle', PLUGIN_URL . 'assets/mystyle.css');
-		wp_enqueue_script('appweb_pluginscript', PLUGIN_URL . 'assets/myscript.js');
+		wp_enqueue_style('appweb_pluginstyle', PLUGIN_URL . 'out/css/appweb_pluginstyle.css');
+		wp_enqueue_script('appweb_pluginscript', PLUGIN_URL . 'out/js/appweb_pluginscript.js');
 	}
 }
